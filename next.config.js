62c49a5bdd1717f794efb802867f833031a0390e
Vladimir Nikodijevic <vladimir.nikodijevic@etondigital.com>
2023-06-12 09:54:49 +0200

    added security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,55 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Adding policies:
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          // force HTTPS
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+            "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          // for older browsers that dont support CSP
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          // CSP needs experimenting
+          // {
+          //   key: "Content-Security-Policy",
+          //   value:
+          //     "default-src 'self'; script-src 'self' 'unsafe-inline'; script-src-elem 'unsafe-inline'; style-src 'unsafe-inline'; style-src-elem 'self' 'unsafe-inline' ",
+          // },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value:
+          //     "default-src 'self' 'http://blog.logrocket.com'; image-src 'self' 'https://unsplash.com'; script-src 'self' 'https://www.google-analytics.com'; font-src 'self' 'https://fonts.googleapis.com'",
+          // },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
